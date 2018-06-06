e40c4f4eef54c0141b63c49686df91f35161c9fc
Konrad Ryczek <konradryczek92@gmail.com>
2018-06-06 18:56:22 +0200

    Add carousel control buttons

--- js/script.js
+++ js/script.js
@@ -7,10 +7,29 @@ $(function(){
 		carouselList.animate({'marginLeft':-1350}, 500, moveFirstSlide);
 	};
 
+	function changeSlideBackward(){
+		carouselList.animate({'marginLeft':0}, 500, moveFirstSlideBack);
+	};
+
 	function moveFirstSlide(){
 		var firstItem = carouselList.find('li:first');
 		var lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
 	};
+
+	function moveFirstSlideBack(){
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft:-1350});
+	};
+
+	$('#right-scroll').click(function() {
+		changeSlide();
+	});
+
+	$('#left-scroll').click(function() {
+		changeSlideBackward();
+	});
 });		
